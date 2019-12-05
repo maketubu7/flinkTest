d1e283040e9ed85748d15ce4d616c7caa7f56c6b
zhengxing <zhengxing@d-bigdata.com>
2019-12-05 23:15:44 +0800

    2

--- src/main/scala/com/bbd/FlinkTest/flinkSource/flinkCourseSourceCode4.scala
+++ src/main/scala/com/bbd/FlinkTest/flinkSource/flinkCourseSourceCode4.scala
@@ -5,36 +5,47 @@ package com.bbd.FlinkTest.flinkSource
   * @Date: 2019/12/5 17:56
   */
 
+import java.util.Random
+
 import org.apache.flink.streaming.api.functions.source.{RichSourceFunction, SourceFunction}
-import com.bbd.FlinkTest.tools.randomTool
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
 
 object flinkCourseSourceCode4 {
   def main(args: Array[String]): Unit = {
     import org.apache.flink.streaming.api.scala._
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    val stream = env.addSource(new Datasource())
+    val stream: DataStream[(String, Int)] = env.addSource(new Datasource())
 
-    stream.addSink(new SinkFunction[(String, Integer)] {
-      override def invoke(value: (String, Integer), context: SinkFunction.Context[_]): Unit = {
-        println(value._1, value._2)
-      }
-    })
+//    stream.addSink(new SinkFunction[(String, Int)] {
+//      override def invoke(value: (String, Int), context: SinkFunction.Context[_]): Unit = {
+//        println("sink" ,value._1, value._2)
+//      }})
+
+    stream.keyBy(0).timeWindow(Time.seconds(5))
+      .reduce((t1:(String, Int), t2:(String, Int)) => {
+      val value = t1._2 + t2._2
+      (t1._1,value)
+    }).print("out: ")
 
     env.execute("source")
   }
 
-  private class Datasource extends RichSourceFunction[Tuple2[String, Integer]]{
+  private class Datasource extends RichSourceFunction[(String, Int)]{
 
     private var runFlag = true
-    override def run(ctx: SourceFunction.SourceContext[(String, Integer)]): Unit = {
+
+    override def run(ctx: SourceFunction.SourceContext[(String, Int)]): Unit = {
+      val random = new Random()
       while (runFlag){
         Thread.sleep(1000)
-        val data = randomTool.getRandomEmit
-//        println(data)
-        ctx.collect(data)
+        val key = "类别" + ('A' + random.nextInt(3)).toChar
+        val value = random.nextInt(10) + 1
+        println("put: ",key, value)
+        ctx.collect((key, value))
       }
 
     }
--- src/main/scala/com/bbd/FlinkTest/tools/randomTool.java
+++ src/main/scala/com/bbd/FlinkTest/tools/randomTool.java
@@ -2,8 +2,8 @@ package com.bbd.FlinkTest.tools;
 
 import scala.Int;
 import scala.Tuple2;
-
 import java.util.Random;
+
 
 /**
  * @Author: maketubu
@@ -15,11 +15,4 @@ public class randomTool {
 
     }
 
-    public static Tuple2<String, Int> getRandomEmit() {
-        Random random = new Random(System.currentTimeMillis());
-        String key = "类别" + (char) ('A' + random.nextInt(3));
-        int value = random.nextInt(10) + 1;
-        Tuple2<String, Int> result = new Tuple2<String, Int>(key, Int);
-        return result;
-    }
 }
