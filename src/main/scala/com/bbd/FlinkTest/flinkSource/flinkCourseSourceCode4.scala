6fa48a2dd4f4684879d864b8487ad56fabded63a
maketubu7 <601176930@qq.com>
2019-12-06 16:38:27 +0800

    1

--- src/main/scala/com/bbd/FlinkTest/flinkSource/flinkCourseSourceCode4.scala
+++ src/main/scala/com/bbd/FlinkTest/flinkSource/flinkCourseSourceCode4.scala
@@ -7,50 +7,118 @@ package com.bbd.FlinkTest.flinkSource
 
 import java.util.Random
 
+import com.bbd.FlinkTest.tools.mysqlTool
+import org.apache.flink.api.common.functions.FoldFunction
+import org.apache.flink.configuration.Configuration
+
+import scala.collection.mutable.HashMap
+import org.apache.log4j.Logger
 import org.apache.flink.streaming.api.functions.source.{RichSourceFunction, SourceFunction}
-import org.apache.flink.streaming.api.functions.sink.SinkFunction
+import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+
+class flinkCourseSourceCode4{}
+
 object flinkCourseSourceCode4 {
   def main(args: Array[String]): Unit = {
+
+    val logger = Logger.getLogger(classOf[flinkCourseSourceCode4])
     import org.apache.flink.streaming.api.scala._
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
     val stream: DataStream[(String, Int)] = env.addSource(new Datasource())
 
-//    stream.addSink(new SinkFunction[(String, Int)] {
-//      override def invoke(value: (String, Int), context: SinkFunction.Context[_]): Unit = {
-//        println("sink" ,value._1, value._2)
-//      }})
+    //    stream.addSink(new SinkFunction[(String, Int)] {
+    //      override def invoke(value: (String, Int), context: SinkFunction.Context[_]): Unit = {
+    //        println("get" ,value._1, value._2)
+    //      }})
 
-    stream.keyBy(0).timeWindow(Time.seconds(5))
-      .reduce((t1:(String, Int), t2:(String, Int)) => {
-      val value = t1._2 + t2._2
-      (t1._1,value)
-    }).print("out: ")
+        stream.keyBy(0).timeWindow(Time.seconds(5))
+          .reduce((t1:(String, Int), t2:(String, Int)) => {
+          val value = t1._2 + t2._2
+          (t1._1,value)
+        }).addSink(new groupCountSink)
+
+//    stream.keyBy(0).sum(1).keyBy(r => {""}).timeWindow(Time.seconds(5)).fold(new mutable.HashMap[String, Int]())(
+//      (accumulator, value) => {
+//        accumulator.put(value._1, value._2)
+//        accumulator
+//      }
+//    ).addSink(new SinkFunction[mutable.HashMap[String, Int]] {
+//      override def invoke(value: mutable.HashMap[String, Int], context: SinkFunction.Context[_]): Unit = {
+//        val sum = value.values.sum
+//        println("outAllCount:", sum)
+//      }
+//    })
+
+//    stream.keyBy(0).sum(1).keyBy(r => {""}).timeWindow(Time.seconds(5)).fold(new mutable.HashMap[String, Int]())(
+//      (accumulator, value) => {
+//        accumulator.put(value._1, value._2)
+//        accumulator
+//      }
+//    ).addSink(new allCountSink)
+
 
     env.execute("source")
   }
 
-  private class Datasource extends RichSourceFunction[(String, Int)]{
+  /** 自定义source */
+  private class Datasource extends RichSourceFunction[(String, Int)] {
 
-    private var runFlag = true
+    private var running = true
 
     override def run(ctx: SourceFunction.SourceContext[(String, Int)]): Unit = {
       val random = new Random()
-      while (runFlag){
+      while (running) {
         Thread.sleep(1000)
-        val key = "类别" + ('A' + random.nextInt(3)).toChar
+        val key = "类别" + ('A' + random.nextInt(4)).toChar
         val value = random.nextInt(10) + 1
-        println("put: ",key, value)
+        println("put: ", key, value)
         ctx.collect((key, value))
       }
 
     }
+
     override def cancel(): Unit = {
-      runFlag = false
+      running = false
     }
   }
+
+
+  /** 自定义sink 总和计入结果 */
+  private class allCountSink extends RichSinkFunction[mutable.HashMap[String, Int]] {
+
+    override def open(parameters: Configuration): Unit = super.open(parameters)
+
+    //value 输入类型
+    override def invoke(value: mutable.HashMap[String, Int], context: SinkFunction.Context[_]): Unit = {
+      val sum = value.values.sum
+      mysqlTool.updateItemSum(sum)
+      println("allItemCount Update Success: %d".format(sum))
+    }
+
+    override def close(): Unit = super.close()
+  }
+
+  /** 自定义sink 分组个数计入结果 */
+  private class groupCountSink extends RichSinkFunction[(String, Int)] {
+
+    override def open(parameters: Configuration): Unit = super.open(parameters)
+
+    //value 输入类型
+    override def invoke(value: (String, Int), context: SinkFunction.Context[_]): Unit = {
+      mysqlTool.updateItemGroupSum(value)
+      println("groupItemCount Update Success: %s-%d".format(value._1, value._2))
+    }
+
+    override def close(): Unit = {
+      mysqlTool.release()
+    }
+  }
+
 }
---
+++ src/main/scala/com/bbd/FlinkTest/tools/mysqlTool.scala
@@ -0,0 +1,69 @@
+package com.bbd.FlinkTest.tools
+
+/**
+  * @Author: maketubu
+  * @Date: 2019/12/6 11:32
+  */
+
+import java.sql.{Connection, DriverManager, PreparedStatement}
+
+/**
+  * MySQL操作工具类
+  */
+class mysqlTool{}
+object mysqlTool {
+
+  var conn: Connection = _
+
+  def updateItemSum(value: Int): Unit ={
+    getConnection()
+    val updateSql = "update itemAllcount set all_item_count = %d".format(value)
+    conn.setAutoCommit(true)
+    val ps = conn.prepareStatement(updateSql)
+    ps.execute()
+  }
+
+  def updateItemGroupSum(value: (String, Int)): Unit ={
+    getConnection()
+    val updateSql = ("INSERT into itemgroupcount(item_name, item_count) " +
+                  "VALUES ('%s', %d) ON DUPLICATE KEY UPDATE item_count = %d").format(value._1, value._2, value._2)
+    conn.setAutoCommit(true)
+    val ps = conn.prepareStatement(updateSql)
+    ps.execute()
+  }
+
+  /**
+    * 获取数据库连接
+    */
+  def getConnection(): Unit = {
+
+    val jdbc = PropertiesTool.getproperties("jdbc","mysql.properties")
+    val user = PropertiesTool.getproperties("user","mysql.properties")
+    val password = PropertiesTool.getproperties("passwd","mysql.properties")
+
+    conn = DriverManager.getConnection(jdbc, user,password)
+
+  }
+
+  /**
+    * 释放数据库连接等资源
+    */
+  def release(): Unit = {
+    try {
+      if (conn != null) {
+        conn.close()
+      }
+    } catch {
+      case e: Exception => e.printStackTrace()
+    } finally {
+        print("")
+      }
+    }
+
+
+  def main(args: Array[String]) {
+//    updateItemSum(67)
+    updateItemGroupSum(("类别C",89))
+  }
+
+}
